69da6af199ae0f247b4dbccd7b850ea4d3194535
Adilius <leugue-gamer@hotmail.com>
2021-03-04 01:23:29 +0100

    Added red wine data set and NB & KNN done

--- main.py
+++ main.py
@@ -5,65 +5,126 @@ from scipy import stats
 from rich.console import Console
 import time
 import matplotlib.pyplot as plt
+import copy #For deep copy
 
-#Creates and prints a roc curve
-def plot_roc_curve(false_positive_rate, true_positive_rate, title):
-    plt.subplots(1, figsize=(10,10))
-    plt.title(title)
-    plt.plot(false_positive_rate, true_positive_rate)
-    plt.plot([0, 1], ls="--")   #Straight line
-    plt.plot([0, 0], [1, 0] , c=".7"), plt.plot([1, 1] , c=".7") #Straight line
-    plt.ylabel('True Positive Rate')
-    plt.xlabel('False Positive Rate')
-    plt.draw()  #Create plot
+from sklearn.model_selection import train_test_split  # Naive bayes
+from sklearn.naive_bayes import GaussianNB  # Naive bayes
+from sklearn import preprocessing  # Naive bayes
+from sklearn import metrics
+from sklearn.neighbors import KNeighborsClassifier
+
+TEST_SIZE = 0.20
+RANDOM_STATE = 40
 
 #Preprocessing
-def preprocessing(df):
+def preprocess(df, rm_dup, rm_out, norm):
 
     #Data reduction
     """" Removes duplicate rows """
-    df.drop_duplicates(inplace=True)
+    if(rm_dup == True):
+        df = df.drop_duplicates()
 
     #Data cleansing
     """ Remove outliers that are more than 3 std from mean """
-    df = df[(np.abs(stats.zscore(df)) < 3).all(axis=1)]
+    if(rm_out == True):
+        df = df[(np.abs(stats.zscore(df)) < 3).all(axis=1)]
 
     #Data transformation
     """ Normalizes each column into a range between 0 and 1 """
-    df = (df-df.min())/(df.max()-df.min())
+    if(norm == True):
+        df.iloc[:, :-1] = df.iloc[:, :-1].apply(lambda x: (x - x.min()) / (x.max() - x.min()))
 
     return df
 
 #Run Naive Bayes on dataframe
 def naive_bayes(df):
 
+    #Split into features and label
     x = df.iloc[:, :-1]
     y = df.iloc[:, -1:]
-    print(x)
-    print(y)
+
+    #Split into training data set and testing data set
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=TEST_SIZE, random_state=RANDOM_STATE)
+
+    #Create Naive Bayes model
+    nb_model = GaussianNB()
+
+    #Train the model
+    nb_model.fit(x_train, y_train.values.ravel())
+
+    #Run prediction
+    y_pred = nb_model.predict(x_test)
+
+    #Create confusion matrix
+    cm = metrics.confusion_matrix(y_test, y_pred)
+
+    #Print confusion matrix
+    print('Confusion matrix NB:')
+    print(cm)
+
+    #Calculate accuracy
+    print('Accuracy NB:' , metrics.accuracy_score(y_test, y_pred))
+
+#Run KNN on dataframe
+def KNN(df):
+
+    #Split into features and label
+    x = df.iloc[:, :-1]
+    y = df.iloc[:, -1:]
+
+    #Split into training data set and testing data set
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=TEST_SIZE, random_state=RANDOM_STATE)
+
+    #Create KNN-model
+    knn_model = KNeighborsClassifier(n_neighbors=5)
+
+    #Train model
+    knn_model.fit(x_train, y_train.values.ravel())
+
+    #Run prediction
+    y_pred = knn_model.predict(x_test)
+
+    #Create confusion matrix
+    cm = metrics.confusion_matrix(y_test, y_pred)
+
+    #Print confusion matrix
+    print('Confusion matrix KNN:')
+    print(cm)
+
+    #Calculate accuracy
+    print('Accuracy KNN:' , metrics.accuracy_score(y_test, y_pred))
 
 
 console = Console()
 
 with console.status('[bold green]Preprocessing data...') as status:
-    df = pd.read_csv('winequality-white.csv', sep=';')
+    time.sleep(1)
+    df = pd.read_csv('winequality-red.csv', sep=';')
     df.columns = [x.strip().replace(' ','_') for x in df.columns]
     # print(df)
-    # print(df.describe())
-    # sns.boxplot(x=df['fixed_acidity'])
-    # plt.show()
+    print(df.describe())
+    #sns.boxplot(x=df['fixed_acidity'])
+    
+    plt.scatter(df['residual_sugar'], df['chlorides'])
+    plt.xlim(0, 20)
+    plt.ylim(-10,10)
+    plt.show()
 
-    df = preprocessing(df)
-
-    # print(df)
-    # print(df.describe())
-    # sns.boxplot(x=df['fixed_acidity'])
-    # plt.show()
-
-
-
+    #Preprocess data set for naive bayes, normalization not needed
+    df_nb = preprocess(copy.deepcopy(df), rm_dup=True, rm_out=False, norm=False)
+    df_knn = preprocess(copy.deepcopy(df), rm_dup=True, rm_out=True, norm=True)
+    plt.scatter(df_knn['residual_sugar'], df_knn['chlorides'])
+    plt.show()
+    print(df_knn.describe())
+    #print('df_nb', df_nb['quality'].describe())
+    #print('df_knn',df_knn['quality'].describe())
 
 
 with console.status('[bold green]Running Naive Bayes...') as status:
-    naive_bayes(df)
-    #time.sleep(3)
+    time.sleep(1)
+    naive_bayes(df_nb)
+
+
+with console.status('[bold green]Running KNN...') as status:
+    time.sleep(1)
+    KNN(df_knn)
